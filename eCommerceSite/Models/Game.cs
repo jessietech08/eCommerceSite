181095b38f3ad4d52ae67620507761acc0e873d4
Jessie S-Tajalle <jessietajalle08@gmail.com>
2024-07-31 20:32:35 -0700

    Added a class

---
+++ eCommerceSite/Models/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single game for available for purchase
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// The unique identifier for each game product
+        /// </summary>
+        [Key]
+        public int GameId { get; set; }
+
+        /// <summary>
+        /// The official title of the video game
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The sales price
+        /// </summary>
+        [Range(0, 1000)]
+        public double Price { get; set; }
+
+        // Todo: add rating
+
+
+    }
+}
