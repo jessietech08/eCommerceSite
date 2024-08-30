b121d5a4838d47afc979ba5bc1644cc4c27c1999
Jessie S-Tajalle <jessietajalle08@gmail.com>
2024-08-29 20:48:13 -0700

    Added viewmodel for login page

--- eCommerceSite/Models/Member.cs
+++ eCommerceSite/Models/Member.cs
@@ -41,4 +41,14 @@ namespace eCommerceSite.Models
 
     }
 
+
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
 }
