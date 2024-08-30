fbf9febd2aa82d7b3e59f7aa3b13efe3dd11ed72
Jessie S-Tajalle <jessietajalle08@gmail.com>
2024-08-29 21:39:31 -0700

    log out functionality added

--- eCommerceSite/Controllers/MembersController.cs
+++ eCommerceSite/Controllers/MembersController.cs
@@ -34,6 +34,8 @@ namespace eCommerceSite.Controllers
                 _context.Members.Add(newMember);
                 await _context.SaveChangesAsync();
 
+                LogUserIn(newMember.Email);
+
                 // Redirect to home page
                 return RedirectToAction("Index", "Home");
             }
@@ -60,7 +62,7 @@ namespace eCommerceSite.Controllers
                 //if exists, send to home page
                 if (m != null)
                 {
-                    HttpContext.Session.SetString("Email", loginModel.Email);
+                    LogUserIn(loginModel.Email);
                     return RedirectToAction("Index", "Home");
                 }
 
@@ -69,6 +71,17 @@ namespace eCommerceSite.Controllers
 
             // if no record matches, display error
             return View(loginModel);
+        }
+
+        private void LogUserIn(string email)
+        {
+            HttpContext.Session.SetString("Email", email);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
